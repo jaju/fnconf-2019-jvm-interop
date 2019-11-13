13e075833d97dd6c280878cba54bbd4e8e6ff6d0
Tamizhvendan S <tamizh88@gmail.com>
2019-11-13 23:20:18 +0530

    add kotlin type-safe html page generator for albums

--- src/main/kotlin/co/gywb/demo/web_front/kotlin/Hello.kt
+++ src/main/kotlin/co/gywb/demo/web_front/kotlin/Hello.kt
@@ -45,4 +45,37 @@ object HelloAlbums {
 
     @JvmStatic fun add(album: Album) = albums.add(album)
     @JvmStatic fun getAll() = albums
+    @JvmStatic fun toHtml() = createHTML().html {
+        head {
+            link(rel="stylesheet", href="bootstrap.min.css")
+        }
+        body {
+            div(classes = "container") {
+                table(classes = "table table-bordered table-striped table-hover") {
+                    thead {
+                        tr {
+                            th(classes = "outline") {+"Number"}
+                            th(classes = "outline") {+"Year"}
+                            th(classes = "outline") {+"Album"}
+                            th(classes = "outline") {+"Artist"}
+                            th(classes = "outline") {+"Genre"}
+                            th(classes = "outline") {+"Subgenre"}
+                        }
+                    }
+                    tbody {
+                        albums.map {
+                            tr {
+                                td { it.number }
+                                td { it.year }
+                                td { it.album }
+                                td { it.artist }
+                                td { it.genre }
+                                td { it.subgenre }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
