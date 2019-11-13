cc9141a2c1836a7e74c739e2ebb57802d19d5e0e
Tamizhvendan S <tamizh88@gmail.com>
2019-11-13 16:31:03 +0530

    add kotlin type-safe html generation

--- src/main/kotlin/co/gywb/demo/web_front/kotlin/Hello.kt
+++ src/main/kotlin/co/gywb/demo/web_front/kotlin/Hello.kt
@@ -1,10 +1,14 @@
 package co.gywb.demo.web_front.kotlin
 
+import co.gywb.demo.web_front.java.Album
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
+import kotlinx.html.*
+import kotlinx.html.dom.*
+import kotlinx.html.stream.createHTML
 
 @Path("/hello/kotlin/{name}")
 class Hello {
@@ -12,4 +16,12 @@ class Hello {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     fun greet(@PathParam("name") name: String) = "Hello, $name, from Kotlin!"
+
+
+    fun albumToHtml(album : Album) : String {
+        return createHTML().div {
+            p { +album.number }
+            p { +album.album }
+        }
+    }
 }
